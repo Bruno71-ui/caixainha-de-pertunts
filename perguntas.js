c5866659d11abb83b7f232bec9195c01e77c66a8
Bruno71-ui <braga.ferreira.bruno@escola.pr.gov.br>
2025-09-23 09:09:05 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+categoria,
+pergunta,
+retopas,
+)
+criaCartao(
+'Quetão 1',
+'Quanto custa uma skin lendaria?',
+'2000 v-bucks.'
+)
+criaCartao(
+'Questão 2',
+'que tipo de jogo é o fortnite?',
+'battlegrounds.'
+)
+criaCartao(
+'Questão 3',
+'fotnite começa com qual letra?',
+'F.'
+)
+criaCartao(
+'Quetão 4',
+'quanta copas do mundo de fortnite solo teve?',
+'6.'
+)
+criaCartao(
+'Questão 5',
+'quantos anos tem o fortnite?',
+'7.'
+)
